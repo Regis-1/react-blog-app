c211ef907282255a62ef392627f3615a6914061c
Jakub Dudzinski <jdudzinski101@gmail.com>
2022-09-29 22:59:43 +0200

    NavBar component added

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <div className="NavBar">
+            <h1>Drone blog</h1>
+            <div className="NavLinks">
+                <a href='/'>Home</a>
+                <a href='/'>Add post</a>
+                <a href='/'>Contact</a>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
