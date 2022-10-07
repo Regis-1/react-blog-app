5cb259cfaf74e0029bf94f8612f65c7e924dc26e
Regis-1 <jdudzinski101@gmail.com>
2022-10-07 15:39:05 +0200

    PageNotFound component added & project over

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import AddPost from "./AddPost";
 import { Routes, Route } from 'react-router-dom';
 import Contact from "./Contact";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/addPost" element={<AddPost />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
